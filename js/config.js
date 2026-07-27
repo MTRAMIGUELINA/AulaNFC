095a2419b99a69682d105ed7644461ee2cce5527
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-07-27 16:03:55 -0600

    Fase 1: agregar configuración modular

---
+++ js/config.js
@@ -0,0 +1,14 @@
+export const CONFIG = Object.freeze({
+  VERSION: "2.0.0",
+  URL_APPS_SCRIPT:
+    "https://script.google.com/macros/s/AKfycbyxSbdzdXQxvVxsPQ8ZA34HoH5U7mR8TpLxYo0sC64X98yAG0POgda76cJq-9YLI46FCg/exec",
+  DUPLICATE_WINDOW_MS: 3000
+});
+
+export const MODULE_NAMES = Object.freeze({
+  asistencia: "Asistencia",
+  tareas: "Tareas",
+  participacion: "Participación",
+  conducta: "Conducta",
+  lectura: "Lectura"
+});
