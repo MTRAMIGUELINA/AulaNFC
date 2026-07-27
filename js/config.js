3b1966277ee1bbdf27a2ccebc1f2e32e1d9a2865
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-07-27 16:12:32 -0600

    Fase 2A: agregar tiempo límite de API

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 export const CONFIG = Object.freeze({
-  VERSION: "2.0.0",
+  VERSION: "2.0.1",
   URL_APPS_SCRIPT:
     "https://script.google.com/macros/s/AKfycbyxSbdzdXQxvVxsPQ8ZA34HoH5U7mR8TpLxYo0sC64X98yAG0POgda76cJq-9YLI46FCg/exec",
-  DUPLICATE_WINDOW_MS: 3000
+  DUPLICATE_WINDOW_MS: 3000,
+  API_TIMEOUT_MS: 12000
 });
 
 export const MODULE_NAMES = Object.freeze({
