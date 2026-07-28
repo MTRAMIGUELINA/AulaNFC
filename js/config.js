d203814e2ae713422b00722b2cc32a94a5609a67
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-07-28 13:01:37 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = Object.freeze({
   VERSION: "2.0.1",
   URL_APPS_SCRIPT:
-    "https://script.google.com/macros/s/AKfycbyxSbdzdXQxvVxsPQ8ZA34HoH5U7mR8TpLxYo0sC64X98yAG0POgda76cJq-9YLI46FCg/exec",
+    "https://script.google.com/macros/s/AKfycbyYdimHGfauQU-z9pwYYdzb60Bcod9lYHgIypsGhhj0Qy9hsQnLJ747IikxKr0El4ObbA/exec",
   DUPLICATE_WINDOW_MS: 3000,
   API_TIMEOUT_MS: 12000
 });
