58e6bfee342367aabd70205874b98c8fb7d4c01e
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-08-08 19:15:53 -0600

    Agregar cargador de resultados de examen

---
+++ resultados-examen-loader.js
@@ -0,0 +1,16 @@
+(() => {
+  if (!document.getElementById('estilosResultadosExamen')) {
+    const link = document.createElement('link');
+    link.id = 'estilosResultadosExamen';
+    link.rel = 'stylesheet';
+    link.href = 'resultados-examen.css?v=1';
+    document.head.appendChild(link);
+  }
+  if (!document.querySelector('script[data-resultados-examen-modulo]')) {
+    const script = document.createElement('script');
+    script.src = 'resultados-examen.js?v=1';
+    script.defer = true;
+    script.dataset.resultadosExamenModulo = 'true';
+    document.body.appendChild(script);
+  }
+})();
