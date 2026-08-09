cd634ec0f52b0d1fc539dc3622a1a8aa06bba159
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-08-09 10:02:17 -0600

    Conectar administración de alumnos al menú

---
+++ administracion-alumnos-loader.js
@@ -0,0 +1,16 @@
+(() => {
+  if (!document.getElementById('estilosAdministracionAlumnos')) {
+    const link=document.createElement('link');
+    link.id='estilosAdministracionAlumnos';
+    link.rel='stylesheet';
+    link.href='administracion-alumnos.css?v=1';
+    document.head.appendChild(link);
+  }
+  if (!document.querySelector('script[data-administracion-alumnos-modulo]')) {
+    const script=document.createElement('script');
+    script.src='administracion-alumnos.js?v=1';
+    script.defer=true;
+    script.dataset.administracionAlumnosModulo='true';
+    document.body.appendChild(script);
+  }
+})();
