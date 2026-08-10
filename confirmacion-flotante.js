778015e937a810a61f7c3b542270a7d74fb0acd6
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-08-10 16:21:26 -0600

    Cargar filtro de grupo activo en registro manual

--- confirmacion-flotante.js
+++ confirmacion-flotante.js
@@ -31,3 +31,4 @@ cargarExtra('script[data-tareas-actividad]','tareas-actividad.js?v=2',{tareasAct
 cargarExtra('script[data-resultados-examen-loader]','resultados-examen-loader.js?v=1',{resultadosExamenLoader:'true'});
 cargarExtra('script[data-administracion-alumnos-loader]','administracion-alumnos-loader.js?v=1',{administracionAlumnosLoader:'true'});
 cargarExtra('script[data-configuracion-loader]','configuracion-loader.js?v=1',{configuracionLoader:'true'});
+cargarExtra('script[data-registro-manual-grupo-activo]','registro-manual-grupo-activo.js?v=1',{registroManualGrupoActivo:'true'});
