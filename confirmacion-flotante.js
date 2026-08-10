353bbfeb940053e10ffbf88eef497fb4cab02865
MTRAMIGUELINA <benitojuarez.mtraangie@gmail.com>
2026-08-10 17:00:35 -0600

    Carga filtro de grupo activo en incidencias

--- confirmacion-flotante.js
+++ confirmacion-flotante.js
@@ -25,6 +25,7 @@ cargarExtra('script[data-dashboard-conexion]','dashboard-conexion.js?v=1',{dashb
 cargarExtra('script[data-reporte-incidencias]','reporte-incidencias.js?v=1',{reporteIncidencias:'true'});
 cargarExtra('script[data-reporte-incidencias-acciones]','reporte-incidencias-acciones.js?v=1',{reporteIncidenciasAcciones:'true'});
 cargarExtra('script[data-reporte-incidencias-config]','reporte-incidencias-config.js?v=1',{reporteIncidenciasConfig:'true'});
+cargarExtra('script[data-reporte-incidencias-grupo-activo]','reporte-incidencias-grupo-activo.js?v=1',{reporteIncidenciasGrupoActivo:'true'});
 cargarExtra('script[data-historial-incidencias]','historial-incidencias.js?v=2',{historialIncidencias:'true'});
 cargarExtra('script[data-participacion-actividad]','participacion-actividad.js?v=2',{participacionActividad:'true'});
 cargarExtra('script[data-tareas-actividad]','tareas-actividad.js?v=2',{tareasActividad:'true'});
